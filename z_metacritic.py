d464faf8ed394f30761f67b8c1c7d28b02d212dc
jc-chuang <62978289+jc-chuang@users.noreply.github.com>
2020-09-23 10:32:09 +0800

    Update and rename o_metacritic.py to z_metacritic.py

--- z_metacritic.py
+++ z_metacritic.py
@@ -4,6 +4,18 @@ from bs4 import BeautifulSoup
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
 }
+
+url = 'https://www.metacritic.com/movie/shoplifters/user-reviews'
+res = requests.get(url,headers=headers)
+print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+review_list = soup.select('div[class="review pad_top1"]')
+for review in review_list:
+    rr = review.select('span[class="blurb blurb_expanded"]')
+    if rr == []:
+        rr = review.select('div[class="review_body"]')
+    print(rr[0].text)
+
 # url = 'https://www.metacritic.com/browse/movies/score/metascore/all/filtered?view=condensed&page=1'
 # res = requests.get(url,headers=headers)
 # print(res)
@@ -24,14 +36,3 @@ headers = {
 #     url = soup.select('tr[class="expand_collapse"] td[class="details"] a')[i]['href']
 #     print(title)
 #     print(url)
-
-url = 'https://www.metacritic.com/movie/shoplifters/user-reviews'
-res = requests.get(url,headers=headers)
-print(res)
-soup = BeautifulSoup(res.text, 'html.parser')
-review_list = soup.select('div[class="review pad_top1"]')
-for review in review_list:
-    rr = review.select('span[class="blurb blurb_expanded"]')
-    if rr == []:
-        rr = review.select('div[class="review_body"]')
-    print(rr[0].text)
